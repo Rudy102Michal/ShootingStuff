9881bb1d7f25bab49438ef4cee5f551ed74fa298
Zax37 <c37zax@gmail.com>
2019-05-03 17:50:55 +0200

    Kind of responsive menu, I guess.

--- ShootingStuff/scripts/menu.gd
+++ ShootingStuff/scripts/menu.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends MarginContainer
 
 export(bool) var menu_visible setget set_menu_visible
 export(bool) var invitation_visible = true setget set_invitation_visible
@@ -8,17 +8,17 @@ var timeout = 0.0
 
 func set_menu_visible(visible):
 	menu_visible = visible
-	$Menu.visible = visible
+	find_node("Menu").visible = visible
 	timeout = 0.1
 	
 func set_invitation_visible(visible):
 	invitation_visible = visible
-	$Invitation.visible = visible
+	find_node("Invitation").visible = visible
 
 func _ready():
-	$Menu.visible = menu_visible
-	$Invitation.visible = invitation_visible
-	$Loading.visible = false
+	find_node("Menu").visible = menu_visible
+	find_node("Invitation").visible = invitation_visible
+	find_node("Loading").visible = false
 	
 func _process(delta):
 	if timeout > 0:
@@ -28,8 +28,8 @@ func _process(delta):
 	
 func _input(event):
 	if (event.is_action("ui_accept") && menu_visible && timeout <= 0):
-		$Menu.visible = false
-		$Invitation.visible = false
-		$Loading.visible = true
+		find_node("Menu").visible = false
+		find_node("Invitation").visible = false
+		find_node("Loading").visible = true
 		started = true
 		timeout = 0.1
