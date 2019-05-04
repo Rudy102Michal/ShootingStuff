d285a18e967e017928abcca8c555080b0994a8fc
adam0314 <adammakowka1@gmail.com>
2019-05-04 20:44:43 +0200

    Added enemy - demogorgon (no animations yet)

---
+++ ShootingStuff/scripts/enemy_manager.gd
@@ -0,0 +1,17 @@
+extends Spatial
+
+# Base file for managing (spawning etc... ) enemies
+const DEMOGORGON = preload("res://models/demog/demogorgon.tscn")
+onready var players_container = get_node("../Players")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	# TODO: this is temporary - it spawns one enemy at (0,0,0)
+	# this will be handled in some loop with proper logic
+	
+	var d = DEMOGORGON.instance()
+	d.set_players_container(players_container)
+	add_child(d)
+	
+	pass
