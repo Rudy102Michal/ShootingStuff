40602b81ec07c5d4eb069caa67f3516da13be49a
Michal Gornicki <mich.gor@wp.pl>
2019-05-24 18:05:37 +0200

    Camera distance calculation fix

--- ShootingStuff/scripts/Isometric_Camera.gd
+++ ShootingStuff/scripts/Isometric_Camera.gd
@@ -7,7 +7,6 @@ var players_container : Spatial
 var distance_translation : Vector3 = Vector3(0.0, 10.0, 10.0)
 
 var camera_angle : float = PI / 6.0
-var camera_distance
 
 #var fov_change = 0.0
 
@@ -23,6 +22,7 @@ func _physics_process(delta):
 	var players : Array = players_container.get_children()
 	var at_target : Vector3
 	var cam_position : Vector3
+	var camera_distance : float = 1.0
 	
 	if players.size() == 1:
 		at_target = players[0].global_transform.origin
@@ -31,17 +31,19 @@ func _physics_process(delta):
 		var players_shift : Vector3 = players[0].global_transform.origin - players[1].global_transform.origin
 		# Point camera at exactly middle between players
 		at_target = players[1].global_transform.origin + players_shift * 0.5
-		cam_position = at_target + distance_translation
 		var distances = []
 		distances.append(players[0].global_transform.origin - global_transform.origin)
 		distances.append(players[1].global_transform.origin - global_transform.origin)
-		var min_dist = min(distances[0].length(), distances[1].length())
-		distances[0] = distances[0].normalized() * min_dist
-		distances[1] = distances[1].normalized() * min_dist
+		var min_dist : Vector3 = distances[0] if distances[0].length() < distances[1].length() else distances[1]
+		var plane_distance : float = min_dist.project(global_transform.basis.z).length()
+		distances[0] = distances[0].normalized() * min_dist.length()
+		distances[1] = distances[1].normalized() * min_dist.length()
 		var projected_player1_pos : Vector2 = unproject_position(global_transform.origin + distances[0])
 		var projected_player2_pos : Vector2 = unproject_position(global_transform.origin + distances[1])
-		var fov_change : float = atan((abs(projected_player1_pos.y - projected_player2_pos.y) + edge_border) / (2 * global_transform.origin.distance_to((distances[0] + distances[1]) * 0.5)));
-		fov = min(max(70, rad2deg(fov_change)), 90)
+		camera_distance = abs(projected_player1_pos.y - projected_player2_pos.y) / (2.0 * tan(deg2rad(fov / 2.0)))
+		#var fov_change : float = atan((abs(projected_player1_pos.y - projected_player2_pos.y) + edge_border) / (2 * global_transform.origin.distance_to((distances[0] + distances[1]) * 0.5)));
+		#fov = min(max(70, rad2deg(fov_change)), 90)
+		cam_position = at_target + distance_translation * max(1.0, camera_distance)
 		
 	look_at_from_position(cam_position, at_target, VECTOR_UP)	
 			
