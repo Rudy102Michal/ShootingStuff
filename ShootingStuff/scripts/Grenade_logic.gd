937ee3f02667f6273c74a54d3e54d3cdd2151433
Michal Gornicki <mich.gor@wp.pl>
2019-09-07 22:06:26 +0200

    Add default timeout for grenade to trigger

--- ShootingStuff/scripts/Grenade_logic.gd
+++ ShootingStuff/scripts/Grenade_logic.gd
@@ -1,8 +1,9 @@
 extends RigidBody
 
 var hit_something : bool = false
-var collision_throw_delay : float = -1.0
 var thrown : bool = false
+var delayed_trigger : float = 2.5	# in seconds
+var delayed_trig_timer : Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,14 +13,16 @@ func _ready():
 func collision(body):
 	hit_something = true
 	print("Grenade hit something, my dude")
-	var parent = get_parent()
-	if parent != null:
-		get_parent().grenade_hit(self)
+	explode()
 
 func set_thrown(value: bool) -> void:
 	thrown = value
 	if thrown:
-		collision_throw_delay = 0.0
+		delayed_trig_timer = Timer.new()
+		delayed_trig_timer.connect("timeout", self, "trigger_timeout_explosion")
+		delayed_trig_timer.set_wait_time(delayed_trigger)
+		add_child(delayed_trig_timer)
+		delayed_trig_timer.start()
 
 func set_hit_something(value: bool) -> void:
 	hit_something = value
@@ -27,3 +30,10 @@ func set_hit_something(value: bool) -> void:
 func set_velocity(vel: Vector3) -> void:
 	apply_central_impulse(vel)
 	
+func trigger_timeout_explosion() -> void:
+	explode()
+	
+func explode() -> void:
+	var parent = get_parent()
+	if parent != null:
+		get_parent().grenade_hit(self)
